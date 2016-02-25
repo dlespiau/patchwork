100977fdc15e44d84c7b7d98a29b96777f5bfb8b
Jose Lamego <jose.a.lamego@linux.intel.com>
2016-02-25 15:27:45 -0800

    settings: add ThreadLocalRequest to Middleware classes
    Signed-off-by: Jose Lamego <jose.a.lamego@linux.intel.com>

--- patchwork/settings/base.py
+++ patchwork/settings/base.py
@@ -36,6 +36,7 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'patchwork.threadlocalrequest.ThreadLocalRequestMiddleware',
 ]
 
 if django.VERSION < (1, 7):
