a20ff49087e538a5f10f7e43963f4aac651d427d
Damien Lespiau <damien.lespiau@intel.com>
2016-03-03 17:11:46 +0000

    tests: Add a script to run all the tests
    Signed-off-by: Damien Lespiau <damien.lespiau@intel.com>

---
+++ tests/test_all.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -e
+
+script_dir=$(cd `dirname $0`; pwd)
+root_dir=`dirname $script_dir`
+tests_dir=$root_dir/tests
+
+update_virtualenv()
+{
+    directory=$1
+    requirements=$2
+
+    [ -d "$directory" ] || virtualenv "$directory"
+    source $directory/bin/activate
+    pip install -r $requirements
+}
+
+update_virtualenv venv $tests_dir/requirements.txt
+
+tox --recreate
+$tests_dir/test_js.sh
+$tests_dir/test_pep8.sh
+
+deactivate
