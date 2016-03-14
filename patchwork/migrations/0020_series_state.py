fba7baa57a62e77f23b3425cf2b60998b47b49b9
Damien Lespiau <damien.lespiau@intel.com>
2016-03-14 17:03:24 +0000

    series: Provide a migrate step that updates series status
    Now that we have the code to update the series status, we can provide a
    step in the migration introducing those fields.
    
    It could have been a separate migration step, but I really don't expect
    anyone to bisect through those commits.
    
    Signed-off-by: Damien Lespiau <damien.lespiau@intel.com>

--- patchwork/migrations/0020_series_state.py
+++ patchwork/migrations/0020_series_state.py
@@ -4,6 +4,20 @@ from __future__ import unicode_literals
 from django.db import migrations, models
 import jsonfield.fields
 
+from patchwork.models import _revision_update_state
+
+
+def noop(apps, schema_editor):
+    pass
+
+
+def update_revision_state(apps, schema_editor):
+    SeriesRevision = apps.get_model("patchwork", "SeriesRevision")
+
+    query = SeriesRevision.objects.all()
+    for _, revision in enumerate(query.iterator()):
+        _revision_update_state(revision)
+
 
 class Migration(migrations.Migration):
 
@@ -22,4 +36,6 @@ class Migration(migrations.Migration):
             name='state_summary',
             field=jsonfield.fields.JSONField(null=True),
         ),
+
+        migrations.RunPython(update_revision_state, noop),
     ]
