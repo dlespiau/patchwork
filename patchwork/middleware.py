51a0dfe203924ed083c4e5ac0ae40d65f226b848
Arkadiusz Hiler <arkadiusz.hiler@intel.com>
2017-10-05 13:23:47 +0300

    middleware: Add CORS allowed headers and include preflight channel
    To have the API cross domain request working properly we also have to
    support the OPTIONS http verb and  allow Content-Type header.
    
    Signed-off-by: Arkadiusz Hiler <arkadiusz.hiler@intel.com>

--- patchwork/middleware.py
+++ patchwork/middleware.py
@@ -1,6 +1,8 @@
 class AccessControlAllowOriginMiddleware:
     """Allow all API GET (read-only) requests from any domain"""
     def process_response(self, request, response):
-        if request.path.startswith('/api/') and request.method == 'GET':
+        if request.path.startswith('/api/') and \
+           (request.method == 'GET' or request.method == 'OPTIONS'):
             response['Access-Control-Allow-Origin'] = '*'
+            response['Access-Control-Allow-Headers'] = 'Content-Type'
         return response
